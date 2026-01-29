330c290a51fb4575c848291ae17a5198d7de419e
Ismail Sinan <ismailsinan2003@gmail.com>
2026-01-29 17:13:13 +0400

    Document Destructuring in JavaScript

---
+++ 03__TypesValues/String/destructure.js
@@ -0,0 +1,10 @@
+// let [x, y] = [1, 2];
+// console.log(x);
+// console.log(y);
+
+// const { max, min } = Math;
+
+const { max: maximum, min: minimum } = Math;
+
+const a = maximum(0, 5);
+console.log(a);
